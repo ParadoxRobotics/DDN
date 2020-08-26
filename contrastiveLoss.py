3ef718f67cdd73437e6e21ddff590c9dcd19a456
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-08-26 14:21:25 +0200

    Update contrastiveLoss.py
    batched contrastive loss with exemple -> seems correct

--- contrastiveLoss.py
+++ contrastiveLoss.py
@@ -12,38 +12,76 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ContrastiveLoss(torch.nn.Module):
-    def __init__(self, margin=0.5, non_match_loss_weight=1.0):
+    def __init__(self, margin=0.5, nonMatchLossWeight=1.0):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
-        self.non_match_loss_weight = non_match_loss_weight
+        self.nonMatchLossWeight = nonMatchLossWeight
 
-    def forward(self, out_A, out_B, match_A, match_B, non_match_A, non_match_B):
-        # out_A, out_B : [1, D, H, W] => [1, H*W, D]
-        # match_A, match_B, non_match_A, non_match_B : [nb_match,] with (u,v)
+    def forward(self, outA, outB, matchA, matchB, nonMatchA, nonMatchB):
+        # outA, outB : [B, D, H, W] => [B, H*W, D]
+        # matchA, matchB, nonMatchA, nonMatchB : [B, nb_match] with (u,v)
         # (u,v) -> image_width * v + u
 
-        # get the number of match/non-match (tensor float)
-        nb_match = match_A.size()[0]
-        nb_non_match = non_match_A.size()[0]
+        # init loss
+        contrastiveLossSum = 0
+        matchLossSum = 0
+        nonMatchLossSum = 0
 
-        # create a tensor with the listed matched descriptors in the estimated descriptors map (net output)
-        match_A_des = torch.index_select(out_A, 1, match_A).unsqueeze(0)
-        match_B_des = torch.index_select(out_B, 1, match_B).unsqueeze(0)
-        # create a tensor with the listed non-matched descriptors in the estimated descriptors map (net output)
-        non_match_A_des = torch.index_select(out_A, 1, non_match_A).unsqueeze(0)
-        non_match_B_des = torch.index_select(out_B, 1, non_match_B).unsqueeze(0)
+        # for every element in the batch
+        for b in range(0,outA.size()[0]):
+            # get the number of match/non-match (tensor float)
+            nbMatch = len(matchA[b])
+            nbNonMatch = len(nonMatchA[b])
+            # create a tensor with the listed matched descriptors in the estimated descriptors map (net output)
+            matchADes = torch.index_select(outA[b].unsqueeze(0), 1, matchA[b]).unsqueeze(0)
+            matchBDes = torch.index_select(outB[b].unsqueeze(0), 1, matchB[b]).unsqueeze(0)
+            # create a tensor with the listed non-matched descriptors in the estimated descriptors map (net output)
+            nonMatchADes = torch.index_select(outA[b].unsqueeze(0), 1, nonMatchA[b]).unsqueeze(0)
+            nonMatchBDes = torch.index_select(outB[b].unsqueeze(0), 1, nonMatchB[b]).unsqueeze(0)
 
-        # calculate match loss (L2 distance)
-        match_loss = 1.0/nb_match * (match_A_des - match_B_des).pow(2).sum()
-        # calculate non-match loss (L2 distance with margin)
-        zeros_vec = torch.zeros_like(non_match_A_des)
-        pixelwise_non_match_loss = torch.max(zeros_vec, self.margin-((non_match_A_des - non_match_B_des).pow(2)))
-        # Hard negative scaling (pixelwise)
-        hard_negative_non_match = len(torch.nonzero(pixelwise_non_match_loss))
-        # final non_match loss with hard negative scaling
-        non_match_loss = self.non_match_loss_weight * 1.0/hard_negative_non_match * pixelwise_non_match_loss.sum()
+            # calculate match loss (L2 distance)
+            matchLoss = 1.0/nbMatch * (matchADes - matchBDes).pow(2).sum()
+            # calculate non-match loss (L2 distance with margin)
+            zerosVec = torch.zeros_like(nonMatchADes)
+            pixelwiseNonMatchLoss = torch.max(zerosVec, self.margin-((nonMatchADes - nonMatchBDes).pow(2)))
+            # Hard negative scaling (pixelwise)
+            hardNegativeNonMatch = len(torch.nonzero(pixelwiseNonMatchLoss))
+            # final non_match loss with hard negative scaling
+            nonMatchloss = self.nonMatchLossWeight * 1.0/hardNegativeNonMatch * pixelwiseNonMatchLoss.sum()
+            # compute contrastive loss
+            contrastiveLoss = matchLoss + nonMatchloss
+            # update final losses
+            contrastiveLossSum += contrastiveLoss
+            matchLossSum += matchLoss
+            nonMatchLossSum += nonMatchloss
 
-        # final contrastive loss
-        contrastive_loss = match_loss + non_match_loss
+        return contrastiveLossSum, matchLossSum, nonMatchLossSum
 
-        return contrastive_loss, match_loss, non_match_loss
+
+"""
+loss = ContrastiveLoss()
+
+batchSize = 12
+# predicted output (640,480 image)
+out_A = torch.randn(batchSize,640*480,3)
+out_B = torch.randn(batchSize,640*480,3)
+
+# match
+match_A = torch.zeros(batchSize,100).type(torch.LongTensor)
+match_B = torch.zeros(batchSize,100).type(torch.LongTensor)
+
+# non_match
+non_match_A = torch.zeros(batchSize, 10).type(torch.LongTensor)
+non_match_B = torch.zeros(batchSize, 10).type(torch.LongTensor)
+
+for b in range(batchSize):
+    # create random (u,v) pair
+    for i in range(0,match_A.size()[1]):
+        match_A[b,i] = int(640*randint(0,127) + randint(0,127))
+        match_B[b,i] = 640*randint(0,127) + randint(0,127)
+    for i in range(0,non_match_A.size()[1]):
+        non_match_A[b,i] = 640*randint(0,127) + randint(0,127)
+        non_match_B[b,i] = 640*randint(0,127) + randint(0,127)
+
+print(loss(out_A, out_B, match_A, match_B, non_match_A, non_match_B))
+"""
